0648c84ab68cae6dec731e81556665b4b07d5d7a
Farah <sumeyafarah1714@gmail.com>
2023-09-06 15:36:42 +0300

    Done and Dead

--- lib/migrations/versions/faebd12f9523_create_db.py
+++ lib/migrations/versions/faebd12f9523_create_db.py
@@ -1,13 +1,7 @@
-"""create db
 
-Revision ID: faebd12f9523
-Revises: 
-Create Date: 2023-03-15 13:20:05.247015
 
-"""
 from alembic import op
 import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision = 'faebd12f9523'
